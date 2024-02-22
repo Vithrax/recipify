92142100e07441549690a3f0e2fd36dcfdebb2d7
Vithrax <adrian.gajdulewicz@outlook.com>
2024-02-22 21:21:30 +0100

    feature: updated t3-env with ut env

--- src/env.js
+++ src/env.js
@@ -34,6 +34,8 @@ export const env = createEnv({
     GITHUB_CLIENT_SECRET: z.string(),
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
+    UPLOADTHING_SECRET: z.string(),
+    UPLOADTHING_APP_ID: z.string(),
   },
 
   /**
@@ -63,6 +65,8 @@ export const env = createEnv({
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     NEXT_PUBLIC_HOSTNAME_URL: process.env.NEXT_PUBLIC_HOSTNAME_URL,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
